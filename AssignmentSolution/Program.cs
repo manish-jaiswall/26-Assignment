9c3939de55df3755b2c216aa0a87a72c65f6463b
Manish Kumar Jaiswal <Manish.Jaiswal@ivycomptech.com>
2022-09-26 17:52:32 +0545

    Add project files.

---
+++ AssignmentSolution/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace AssignmentSolution
+{
+    public class Student<T>
+    {
+        public void display(int id, T name, T pass)
+        {
+            Console.WriteLine("[" + id + "," + name + "," + pass + "]");
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Student<string> student = new Student<string>();
+            student.display(1, "Hari", "Y");
+            student.display(2, "Manish", "Y");
+            student.display(3, "Sita", "N");
+            student.display(4, "Geeta", "Y");
+            student.display(5, "Niraj", "Y");
+        }
+    }
+}
