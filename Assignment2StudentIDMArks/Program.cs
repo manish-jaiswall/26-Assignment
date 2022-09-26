1f6027156438342c4d644db63f0be6265d8156dd
Manish Kumar Jaiswal <Manish.Jaiswal@ivycomptech.com>
2022-09-26 17:07:50 +0545

    Add project files.

---
+++ Assignment2StudentIDMArks/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace Assignment2StudentIDMArks
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the number of students you want to store");
+            int n=Convert.ToInt32(Console.ReadLine());
+            var si=new SortedDictionary<int,int>();
+
+            for(int i=0;i<n;i++)
+            {
+                Console.WriteLine("Enter the id of the student");
+                int id=Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Enter the marks of the student");
+                int marks=Convert.ToInt32(Console.ReadLine());  
+
+                si.Add(id, marks);
+            }
+            Console.WriteLine("The list of students are: ");
+
+            foreach(KeyValuePair<int,int> pair in si)
+            {
+                Console.WriteLine(pair.Key+" "+pair.Value); ;
+            }
+
+        }
+    }
+}
